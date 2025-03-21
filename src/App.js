6af19dcd7aea7552101c9a1b00e61d6e84f60998
Ishita Pandya <ishitapandya1203@gmail.com>
2025-03-21 14:09:38 -0400

    App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from 'react'
 import {Route, Routes} from 'react-router-dom'
 import {Box} from "@mui/material";
+
 import './App.css';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import ExerciseDetail from './pages/ExerciseDetail';
+
 
 const App = () => {
   return (
